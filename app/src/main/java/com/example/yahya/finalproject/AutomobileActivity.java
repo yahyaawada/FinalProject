c106013b809611a85a69d816276bcae3e27c49d3
yahya <awad0083@algonquinlive.com>
2017-12-07 12:48:49 -0500

    milestone 1

--- app/src/main/java/com/example/yahya/finalproject/AutomobileActivity.java
+++ app/src/main/java/com/example/yahya/finalproject/AutomobileActivity.java
@@ -2,12 +2,21 @@ package com.example.yahya.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AutomobileActivity extends Activity {
+
+    String [] sourceData = { "  ","Activity Tracking", "  ", "Food Nutrition information tracker",
+            "House Thermostat", "Automobile"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_automobile);
+
+        //Creating an array adapter for the listView
+        ListView lv = (ListView)findViewById(R.id.automobile_list);
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.cell_layout, sourceData));
     }
 }
